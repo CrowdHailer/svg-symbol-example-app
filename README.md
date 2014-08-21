a67edbcb309ee2e447ccc1647d407bfbdff7763f
Peter <peterhsaxton@gmail.com>
2014-08-21 15:07:56 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+SVG Symbol Example App
+======================
+
+This application is an example workflow for included svg's as a single file of symbol definitions.
+
+Resources
+=========
+
+these are some resources as to why this is a good idea
+http://css-tricks.com/svg-symbol-good-choice-icons/
+http://css-tricks.com/video-screencasts/screencast-134-tour-site-progress-built-jekyll-grunt-sass-svg-system/
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var svgmin = require('gulp-svgmin');
+var svgstore = require('gulp-svgstore');
+var inject = require('gulp-inject');
+
+gulp.task('build', function () {
+    gulp.src(['app/config.xml', 'app/index.html'], {base: 'app/'})
+        .pipe(gulp.dest('www'))
+});
+
+function fileContents (filePath, file) {
+    return file.contents.toString('utf8');
+}
+
+gulp.task('svg', function () {
+    var svgs = gulp.src(['app/resources/*.svg', '!app/resources/lines.svg'])
+        .pipe(svgmin())
+        .pipe(svgstore({inlineSvg: true}))
+
+    return gulp.src('app/index.html')
+        .pipe(inject(svgs, { transform: fileContents }))
+        .pipe(gulp.dest('www/'));
+});

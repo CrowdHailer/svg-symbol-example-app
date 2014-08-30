38cab68872b52d34f83f0e5053e95f2fe5c54a14
Peter <peterhsaxton@gmail.com>
2014-08-30 14:50:06 +0100

    add svg transform

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,15 @@ function fileContents (filePath, file) {
     return file.contents.toString('utf8');
 }
 
+function transformSvg (svg, cb) {
+    svg.attr({ style: 'display:none' })
+    cb(null)
+}
+
 gulp.task('svg', function () {
     var svgs = gulp.src(['app/resources/*.svg', '!app/resources/lines.svg'])
         .pipe(svgmin())
-        .pipe(svgstore({inlineSvg: true}))
+        .pipe(svgstore({inlineSvg: true, transformSvg: transformSvg}))
 
     return gulp.src('app/index.html')
         .pipe(inject(svgs, { transform: fileContents }))

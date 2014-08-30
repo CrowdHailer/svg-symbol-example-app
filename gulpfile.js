771d49569a506fc5775fd503b94ac2199e058af6
Peter <peterhsaxton@gmail.com>
2014-08-30 17:12:51 +0100

    Add symbol styling

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,33 @@ var gulp = require('gulp');
 var svgmin = require('gulp-svgmin');
 var svgstore = require('gulp-svgstore');
 var inject = require('gulp-inject');
+var del = require('del');
 
-gulp.task('build', function () {
-    gulp.src(['app/config.xml', 'app/index.html'], {base: 'app/'})
-        .pipe(gulp.dest('www'))
-});
-
+// Returns File object as string, to inject in html file
 function fileContents (filePath, file) {
     return file.contents.toString('utf8');
 }
 
+// Add's display none propery to svg with definitions
 function transformSvg (svg, cb) {
     svg.attr({ style: 'display:none' })
     cb(null)
 }
 
+// Remove the distribution file
+gulp.task('clean', function () {
+    del.sync('www')
+});
+
+// Copy files to www that do not require transform
+gulp.task('copy', function () {
+    return gulp.src('app/config.xml', {base: 'app/'})
+        .pipe(gulp.dest('www'))
+});
+
+// Builds SVG store from all symbol resources
 gulp.task('svg', function () {
-    var svgs = gulp.src(['app/resources/*.svg', '!app/resources/lines.svg'])
+    var svgs = gulp.src('app/resources/symbols/*.svg')
         .pipe(svgmin())
         .pipe(svgstore({inlineSvg: true, transformSvg: transformSvg}))
 
@@ -26,3 +36,5 @@ gulp.task('svg', function () {
         .pipe(inject(svgs, { transform: fileContents }))
         .pipe(gulp.dest('www/'));
 });
+
+gulp.task('default', ['clean', 'copy', 'svg'])
